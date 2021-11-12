7c35a058d7a4a7ada5b51df89b07395be54b77a9
rajbrady <raj@alan.app>
2021-11-11 19:25:33 -0500

    First Changes

---
+++ Downloads/OSX Developer Tools 3.3/EEG_Algo_SDK_OSX/Algo SDK Sample/Algo SDK Sample/TableVC.swift
@@ -0,0 +1,13 @@
+//
+//  TableVC.swift
+//  Algo SDK Sample
+//
+//  Created by Raj Janardhan on 11/11/21.
+//  Copyright © 2021 NeuroSky. All rights reserved.
+//
+
+import Cocoa
+
+class TableVC: NSObject {
+
+}
